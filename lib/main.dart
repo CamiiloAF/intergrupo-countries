10cf6025b391592332cbd4095edd65df9ded45b3
Camilo Agudelo <camiiloagudelo92@gmail.com>
2022-06-27 17:38:42 -0500

    Update SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'core/environments/config_environment.dart';
 import 'shared/routes/app_routes.dart';
@@ -9,7 +10,9 @@ Future<void> mainCommon({
   required final bool debug,
   required final Function injectionDependencies,
 }) async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   await ConfigEnvironments.initialize(env: env);
 
@@ -23,6 +26,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(final BuildContext context) {
+    Future.delayed(const Duration(seconds: 1), FlutterNativeSplash.remove);
+
     return const MaterialApp(
       title: 'Intergrupo',
       initialRoute: AppRoutes.homeScreen,
